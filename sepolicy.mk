6a2a01f8779c8834518db7e491f882a5fbb2151c
Pig <pig.priv@gmail.com>
2020-09-19 09:39:06 +0800

    sepolicy: Include Xtended-specific QCOM sepolicy
    Change-Id: Ibf70e4c8ab9d91b50c62c3e9f1263e1624e8ca00
    Signed-off-by: TogoFire <italomellopereira@gmail.com>

--- sepolicy.mk
+++ sepolicy.mk
@@ -27,4 +27,4 @@ BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/qcom/sepolicy-legacy-um/private
 
--include device/legion/sepolicy/qcom/sepolicy.mk
+-include device/xtended/sepolicy/qcom/sepolicy.mk
